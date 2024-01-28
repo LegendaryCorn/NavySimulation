a78d63785dc557d96f7a18699dd17828d8905432
Liam Francisco <98508195+francisco-liam@users.noreply.github.com>
2024-01-28 09:49:46 -0800

    visuals w/o color

--- Assets/GraphPlane.cs
+++ Assets/GraphPlane.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
 using UnityEngine;
-//using UnityEngine.Experimental.PlayerLoop;
-using Color = UnityEngine.Color;
 
+//struct to hold individual ship data
+public struct ShipData
+{
+    public Vector3 position;
+    public Vector3 velocity;
+    public Vector3 movePosition;
+    public float heading;
+};
 
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshFilter))]
@@ -14,18 +18,34 @@ public class GraphPlane : MonoBehaviour
     Mesh mesh;
     MeshFilter meshFilter;
 
-    List<Vector3> vertices;
+    public List<Vector3> vertices;
     List<int> triangles;
-
+    List<ShipData> allShipData; //list for individual ship data
+    public ComputeShader potentialShader; //compute shader used for calculation
     public Vector2 size;
     public int resolution;
-    public SimulatedEntity sEntity;
+    public SimulatedEntity sEntity; //entity that the graph is calculating for
 
     void Awake()
     {
+        //Create and set mesh
         mesh = new Mesh();
         meshFilter = GetComponent<MeshFilter>();
         meshFilter.mesh = mesh;
+
+        //Change mesh format, this needs to be done for graphs with large resolutions
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+
+        //Set mesh size and resolutions
+        size = GraphMgr.inst.size;
+        resolution = GraphMgr.inst.resolution;
+        resolution = Mathf.Clamp(resolution, 0, 1000);
+
+        //Creates and sets triangles and vertices of the mesh
+        GeneratePlane(size, resolution);
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        allShipData = new List<ShipData>();
     }
 
     // Start is called before the first frame update
@@ -38,10 +58,8 @@ public class GraphPlane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        resolution = Mathf.Clamp(resolution, 0, 50);
-        GeneratePlane(size, resolution);
-        UpdateHeights();
-        AssignMesh();
+        UpdateHeights(sEntity.ent);
+        UpdateMesh();
     }
 
     void SetSizeAndResolutiion()
@@ -82,29 +100,66 @@ public class GraphPlane : MonoBehaviour
         }
     }
 
-    void AssignMesh()
+    void UpdateMesh()
     {
-        mesh.Clear();
         mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.colors = ChangeColors();
         mesh.RecalculateBounds();
         mesh.RecalculateNormals();
         mesh.RecalculateTangents();
     }
 
-    void UpdateHeights()
+    void UpdateHeights(Entity381 entity)
     {
-        for (int i = 0; i < vertices.Count; i++)
+        updateShipData();
+        ComputeBuffer shipsBuffer = new ComputeBuffer(allShipData.Count, sizeof(float) * 10);
+        shipsBuffer.SetData(allShipData);
+
+        //sends world position of mesh vertices to the compute shader via compute buffer
+        ComputeBuffer vertexBuffer = new ComputeBuffer(vertices.Count, sizeof(float) * 3);
+        Vector3[] worldVert = vertices.ToArray();
+        transform.TransformPoints(worldVert); //transforms local mesh coordinates to world cordinates
+        vertexBuffer.SetData(worldVert);
+
+        //sends potential parameters to compute shader via compute buffer
+        List<PotentialParameters> potParms = new List<PotentialParameters>();
+        PotentialParameters pf = entity.gameMgr.aiMgr.potentialParameters;
+        potParms.Add(pf);
+        ComputeBuffer potentialBuffer = new ComputeBuffer(1, sizeof(float) * 14);
+        potentialBuffer.SetData(potParms);
+
+        //sets variables in the compute shader
+        potentialShader.SetInt("numShips", entity.gameMgr.entityMgr.entities.Count);
+        potentialShader.SetInt("entity", entity.gameMgr.entityMgr.entities.IndexOf(entity));
+        potentialShader.SetInt("entCommands", entity.ai.commands.Count);
+        potentialShader.SetFloat("maxMag", GraphMgr.inst.maxMag);
+        potentialShader.SetBuffer(0, "ships", shipsBuffer);
+        potentialShader.SetBuffer(0, "positions", vertexBuffer);
+        potentialShader.SetBuffer(0, "potential", potentialBuffer);
+
+        //starts calculations in buffer
+        potentialShader.Dispatch(0, (vertices.Count / 64) + 1, 1, 1);
+
+        //retrieves calculations and sets new vertices
+        vertexBuffer.GetData(worldVert);
+        transform.InverseTransformPoints(worldVert); //transforms world coordinates back to local coordinates
+        vertices = new List<Vector3>(worldVert);
+
+        //disposes buffers after use
+        shipsBuffer.Dispose();
+        vertexBuffer.Dispose();
+        potentialBuffer.Dispose();
+
+        /*for (int i = 0; i < vertices.Count; i++)
         {
             Vector3 vertex = vertices[i];
             Vector3 vertexPos = transform.TransformPoint(vertex);
             vertexPos.y = 0;
             vertex.y = (CalculatePotential(vertexPos, sEntity) / GraphMgr.inst.maxMag) * 400;
             vertices[i] = vertex;
-        }
+        }*/
     }
 
+    /*
     float CalculatePotential(Vector3 position, SimulatedEntity entity)
     {
         float magnitude;
@@ -129,7 +184,7 @@ public class GraphPlane : MonoBehaviour
 
         magnitude = Utils.Clamp(repTotal.magnitude - attTotal.magnitude, -GraphMgr.inst.maxMag, GraphMgr.inst.maxMag);
 
-        return magnitude;
+        return 0;
 
     }
 
@@ -144,4 +199,23 @@ public class GraphPlane : MonoBehaviour
 
         return colors;
     }
+    */
+    public void updateShipData()
+    {
+        allShipData = new List<ShipData>();
+        foreach (Entity381 entity in sEntity.ent.gameMgr.entityMgr.entities)
+        {
+            ShipData shipData = new ShipData();
+            shipData.position = entity.position;
+            shipData.velocity = entity.velocity;
+            shipData.heading = entity.heading;
+            if (entity.ai.commands.Count != 0)
+                shipData.movePosition = entity.ai.moves[0].movePosition;
+            else
+                shipData.movePosition = Vector3.zero;
+            allShipData.Add(shipData);
+        }
+    }
+
+
 }
